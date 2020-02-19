4178d67c266fb5510198bcdb0f0a8f3db890bc3e
svensktutby <svensk.tut.by@gmail.com>
2020-02-20 00:48:26 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array && array.length ?
+    array.slice().sort((a, b) => a - b)[0] :
+    0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array && array.length ?
+    array.slice().sort((a, b) => b - a)[0] :
+    0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array && array.length ?
+    array.slice().reduce((acc, item) => acc + item,0) / array.length :
+    0;
 }
